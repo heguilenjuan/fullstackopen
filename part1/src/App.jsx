9028662cadde1e2856770e1cc700dd1e4ba9bed8
heguilenjuan <heguilenjuan@gmail.com>
2026-02-06 15:35:22 -0300

    exercise 1.13

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,44 +1,40 @@
-import { useState } from "react";
-import Button from "./Button";
-import Statistic from "./Statistic";
+import { useState } from 'react'
+import Button from './Button'
+
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  let total = good + neutral + bad;
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
-  const handleGood = () => {
-    setGood(good + 1)
-  }
-  const handleNeutral = () =>{
-    setNeutral(neutral + 1)
-  }
-
-  const handleBad = () => {
-    setBad(bad + 1)
-  }
-  const calculateAvergate = () => {
-    if(good === 0) return -1
-    let sumary = good - bad;
-    return sumary/total
+  const handleNextAnecdote = () => {
+    setSelected(selected + 1)
   }
 
-  const percentPositive = () => {
-    if(good === 0) return 0
-    return (good/total)*100 + "%"
+  const applyVote = () => {
+      const newVotes = [...votes]
+      newVotes[selected] += 1
+      setVotes(newVotes)
   }
-  
+
   return (
+    <>
     <div>
-      <h3>give feedback</h3>
-      <Button name={"good"} onClick={handleGood}/>
-      <Button name={"neutral"} onClick={handleNeutral}/>
-      <Button name={"bad"} onClick={handleBad}/>
-      <h3>statistics</h3>
-      <Statistic good={good} neutral={neutral} bad={bad} total={total} 
-      Average={calculateAvergate()} Positive={percentPositive()} />
+      {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
     </div>
+      <Button name={"vote"} onClick={applyVote}/>
+      <Button name={"next anecdote"} onClick={handleNextAnecdote}/>
+    </>
   )
 }
 
